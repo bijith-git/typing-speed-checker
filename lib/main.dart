5ee693bd718c93424b33e803056e997d7a9591f8
Bijith <bijithpn@gmail.com>
2024-02-20 22:56:32 +0530

    firebase and inital loader added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
   doWhenWindowReady(() {
     appWindow.minSize = const Size(400, 500);
